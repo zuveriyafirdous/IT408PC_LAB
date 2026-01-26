4c3e6d77255b1f022202f981dee84fc80c67e181
zuveriyafirdous <zuveriyafirdous5@gmail.com>
2026-01-26 22:25:17 +0530

    Add files via upload

---
+++ L1P3.java
@@ -0,0 +1,13 @@
+class L1P3{
+	public static void main(String...args){
+		char ch='a';
+		int x=10;
+		short s=50;
+		long l=456;
+		float fl=30f;
+		double d=x;
+		byte z=12;
+		boolean b=false;
+		System.out.println("integer Value: "+x+" short: "+s+" long: "+l+" float: "+fl+" double: "+d+" byte: "+z+" boolean: "+b);
+	}
+}
