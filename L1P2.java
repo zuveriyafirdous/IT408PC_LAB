0804e14a03ebb0126c8f31f3c06e24b3cb38a398
zuveriyafirdous <zuveriyafirdous5@gmail.com>
2026-01-26 22:24:53 +0530

    Add files via upload

---
+++ L1P2.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class L1P2{
+	public static void main(String...args){
+		int x=50;
+		double d=x;
+		System.out.println("Integer value: "+x+"Double value: "+d);
+		double b =(int)d;
+		System.out.println("Integer value: "+b);
+	}
+}
